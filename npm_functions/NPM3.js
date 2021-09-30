4d6cb3375a2239d0716bdabc78c1dda2e3916c43
Alan Licerio <alicerio2@miners.utep.edu>
2021-09-30 13:10:26 -0600

    Added NPM 3-2 and 3-3. Methodology is pending.

--- npm_functions/NPM3.js
+++ npm_functions/NPM3.js
@@ -24,8 +24,8 @@ function npm3_1tx() {
                 data: [88.4, 88.3, 91.2, 98.3],
                 backgroundColor: 'blue',
                 borderColor: 'blue',
-                borderWidth: 1,
-                pointRadius: 5
+                borderWidth: 2,
+                pointRadius: 3
             },
             {
                 type: 'line',
@@ -33,12 +33,12 @@ function npm3_1tx() {
                 data: [79.6, 79.6, 79.6, 79.6],
                 backgroundColor: 'orange',
                 borderColor: 'orange',
-                borderWidth: 1,
-                pointRadius: 5
+                borderWidth: 2,
+                pointRadius: 0
             },
             {
                 type: 'scatter',
-                label: 'EPMPO Target - 2020',
+                label: 'TXDOT Target - 2020',
                 data: [null, null, null, 61.2],
                 backgroundColor: 'black',
                 pointStyle: 'rectRot',
@@ -46,7 +46,7 @@ function npm3_1tx() {
             },
             {
                 type: 'scatter',
-                label: 'EPMPO Target - 2022',
+                label: 'TXDOT Target - 2022',
                 data: [null, null, null, null, null, 56.6],
                 backgroundColor: 'red',
                 pointStyle: 'rect',
@@ -91,7 +91,7 @@ function npm3_1tx() {
                     }
                 },
                 title: {
-                    text: "Person-miles Traveled",
+                    text: "Person-Miles Traveled",
                     display: true,
                     padding: 1,
                     font: { weight: 'bold' }
@@ -118,8 +118,8 @@ function npm3_1nm() {
                 data: [100,100,100,100],
                 backgroundColor: 'blue',
                 borderColor: 'blue',
-                borderWidth: 1,
-                pointRadius: 5
+                borderWidth: 2,
+                pointRadius: 3
             },
             {
                 type: 'line',
@@ -127,12 +127,12 @@ function npm3_1nm() {
                 data: [79.6, 79.6, 79.6, 79.6],
                 backgroundColor: 'orange',
                 borderColor: 'orange',
-                borderWidth: 1,
-                pointRadius: 5
+                borderWidth: 2,
+                pointRadius: 0
             },
             {
                 type: 'scatter',
-                label: 'EPMPO Target - 2020',
+                label: 'NMDOT Target - 2020',
                 data: [null, null, null, 61.2],
                 backgroundColor: 'black',
                 pointStyle: 'rectRot',
@@ -140,7 +140,7 @@ function npm3_1nm() {
             },
             {
                 type: 'scatter',
-                label: 'EPMPO Target - 2022',
+                label: 'NMDOT Target - 2022',
                 data: [null, null, null, null, null, 56.6],
                 backgroundColor: 'red',
                 pointStyle: 'rect',
@@ -185,7 +185,349 @@ function npm3_1nm() {
                     }
                 },
                 title: {
-                    text: "Person-miles Traveled",
+                    text: "Person-Miles Traveled",
+                    display: true,
+                    padding: 1,
+                    font: { weight: 'bold' }
+                },
+                chartAreaBorder: {
+                    borderColor: "black",
+                    borderWidth: 2,
+                }
+            }
+        },
+        plugins: [chartAreaBorder]
+    });
+}
+
+function npm3_2tx() {
+    Chart.defaults.font.size = 18;
+    Chart.defaults.color = "black";
+    var ctx = document.getElementById('Tx_NPM3-2');
+    var myChart = new Chart(ctx, {
+        data: {
+            datasets: [{
+                type: 'line',
+                label: 'Annual - TX',
+                data: [79.2, 76.7, 83.1, 86.7],
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                borderWidth: 2,
+                pointRadius: 3
+            },
+            {
+                type: 'scatter',
+                label: 'TXDOT Target - 2022',
+                data: [null, null, null, null, null, 55.4],
+                backgroundColor: 'red',
+                pointStyle: 'rect',
+                pointRadius: 7
+            }],
+            labels: [2017, 2018, 2019, 2020, 2021, 2022, 2023]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Year",
+                        font: {
+                            weight: 'bold'
+                        }
+                    }
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Travel Time Reliability Measure",
+                        font: {
+                            weight: 'bold'
+                        }
+                    },
+                    min: 0,
+                    max: 100,
+                    ticks: {
+                        stepSize: 25
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    position: "bottom",
+                    labels: {
+                        usePointStyle: true
+                    }
+                },
+                title: {
+                    text: "Person-Miles Traveled",
+                    display: true,
+                    padding: 1,
+                    font: { weight: 'bold' }
+                },
+                chartAreaBorder: {
+                    borderColor: "black",
+                    borderWidth: 2,
+                }
+            }
+        },
+        plugins: [chartAreaBorder]
+    });
+}
+
+function npm3_2nm() {
+    Chart.defaults.font.size = 18;
+    Chart.defaults.color = "black";
+    var ctx = document.getElementById('Nm_NPM3-2');
+    var myChart = new Chart(ctx, {
+        data: {
+            datasets: [{
+                type: 'line',
+                label: 'Annual - NM',
+                data: [100, 100, 80.7, 91.9],
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                borderWidth: 2,
+                pointRadius: 3
+            },
+            {
+                type: 'scatter',
+                label: 'NMDOT Target - 2022',
+                data: [null, null, null, null, null, 55.4],
+                backgroundColor: 'red',
+                pointStyle: 'rect',
+                pointRadius: 7
+            }],
+            labels: [2017, 2018, 2019, 2020, 2021, 2022, 2023]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Year",
+                        font: {
+                            weight: 'bold'
+                        }
+                    }
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Travel Time Reliability Measure",
+                        font: {
+                            weight: 'bold'
+                        }
+                    },
+                    min: 0,
+                    max: 100,
+                    ticks: {
+                        stepSize: 25
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    position: "bottom",
+                    labels: {
+                        usePointStyle: true
+                    }
+                },
+                title: {
+                    text: "Person-Miles Traveled",
+                    display: true,
+                    padding: 1,
+                    font: { weight: 'bold' }
+                },
+                chartAreaBorder: {
+                    borderColor: "black",
+                    borderWidth: 2,
+                }
+            }
+        },
+        plugins: [chartAreaBorder]
+    });
+}
+
+function npm3_3tx() {
+    Chart.defaults.font.size = 18;
+    Chart.defaults.color = "black";
+    var ctx = document.getElementById('Tx_NPM3-3');
+    var myChart = new Chart(ctx, {
+        data: {
+            datasets: [{
+                type: 'line',
+                label: 'Annual - TX',
+                data: [1.54, 1.49, 1.47, 1.26],
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                borderWidth: 2,
+                pointRadius: 3
+            },
+            {
+                type: 'line',
+                label: "Baseline",
+                data: [1.50, 1.50, 1.50, 1.50],
+                backgroundColor: 'orange',
+                borderColor: 'orange',
+                borderWidth: 2,
+                pointRadius: 0
+            },
+            {
+                type: 'scatter',
+                label: 'TXDOT Target - 2020',
+                data: [null, null, null, 1.70],
+                backgroundColor: 'black',
+                pointStyle: 'rectRot',
+                pointRadius: 7
+            },
+            {
+                type: 'scatter',
+                label: 'TXDOT Target - 2022',
+                data: [null, null, null, null, null, 1.79],
+                backgroundColor: 'red',
+                pointStyle: 'rect',
+                pointRadius: 7
+            }],
+            labels: [2017, 2018, 2019, 2020, 2021, 2022, 2023]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Year",
+                        font: {
+                            weight: 'bold'
+                        }
+                    }
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Truck Travel Time Reliability (TTTR)",
+                        font: {
+                            weight: 'bold'
+                        }
+                    },
+                    min: 1.00,
+                    max: 1.80,
+                    ticks: {
+                        stepSize: 0.10
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    position: "bottom",
+                    labels: {
+                        usePointStyle: true
+                    }
+                },
+                title: {
+                    text: "Truck Travel Time Reliability",
+                    display: true,
+                    padding: 1,
+                    font: { weight: 'bold' }
+                },
+                chartAreaBorder: {
+                    borderColor: "black",
+                    borderWidth: 2,
+                }
+            }
+        },
+        plugins: [chartAreaBorder]
+    });
+}
+
+function npm3_3nm() {
+    Chart.defaults.font.size = 18;
+    Chart.defaults.color = "black";
+    var ctx = document.getElementById('Nm_NPM3-3');
+    var myChart = new Chart(ctx, {
+        data: {
+            datasets: [{
+                type: 'line',
+                label: 'Annual - NM',
+                data: [1.13, 1.14, 1.17, 1.13],
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                borderWidth: 2,
+                pointRadius: 3
+            },
+            {
+                type: 'line',
+                label: "Baseline",
+                data: [1.50, 1.50, 1.50, 1.50],
+                backgroundColor: 'orange',
+                borderColor: 'orange',
+                borderWidth: 2,
+                pointRadius: 0
+            },
+            {
+                type: 'scatter',
+                label: 'NMDOT Target - 2020',
+                data: [null, null, null, 1.70],
+                backgroundColor: 'black',
+                pointStyle: 'rectRot',
+                pointRadius: 7
+            },
+            {
+                type: 'scatter',
+                label: 'NMDOT Target - 2022',
+                data: [null, null, null, null, null, 1.79],
+                backgroundColor: 'red',
+                pointStyle: 'rect',
+                pointRadius: 7
+            }],
+            labels: [2017, 2018, 2019, 2020, 2021, 2022, 2023]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Year",
+                        font: {
+                            weight: 'bold'
+                        }
+                    }
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Truck Travel Time Reliability (TTTR)",
+                        font: {
+                            weight: 'bold'
+                        }
+                    },
+                    min: 1.00,
+                    max: 1.80,
+                    ticks: {
+                        stepSize: 0.10
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    position: "bottom",
+                    labels: {
+                        usePointStyle: true
+                    }
+                },
+                title: {
+                    text: "Truck Travel Time Reliability",
                     display: true,
                     padding: 1,
                     font: { weight: 'bold' }
